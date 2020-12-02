48c79c0c03317bbd087f169bbcb48a96e29d3572
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2020-12-02 16:42:15 +0530

    [Saiprasad]Added: Method pop for deleting head of the given linked list

--- main/java/MyLinkedList.java
+++ main/java/MyLinkedList.java
@@ -49,4 +49,10 @@ public class MyLinkedList {
         myNode.setNext(newNode);
         newNode.setNext(tempNode);
     }
+
+    public INode pop() {
+        INode tempNode = this.head;
+        this.head = head.getNext();
+        return tempNode;
+    }
 }
--- test/java/MyLinkedListTest.java
+++ test/java/MyLinkedListTest.java
@@ -49,4 +49,20 @@ public class MyLinkedListTest {
                 myLinkedList.tail.equals(myThirdNode);
         Assert.assertTrue(result);
     }
+
+    @Test
+    public void poppingFirstElement() {
+        MyNode<Integer> myFirstNode = new MyNode<Integer>(56);
+        MyNode<Integer> mySecondNode = new MyNode<Integer>(30);
+        MyNode<Integer> myThirdNode = new MyNode<Integer>(70);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.addAtBottom(myFirstNode);
+        myLinkedList.addAtBottom(mySecondNode);
+        myLinkedList.addAtBottom(myThirdNode);
+        myLinkedList.pop();
+        myLinkedList.printMyNodes();
+        boolean result = myLinkedList.head.equals(mySecondNode) &&
+                        myLinkedList.tail.equals(myThirdNode);
+        Assert.assertTrue(result);
+    }
 }
