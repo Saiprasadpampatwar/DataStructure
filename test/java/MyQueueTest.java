078f3ede2f9c53296a06372a041c3bf21b131d14
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2020-12-03 18:02:32 +0530

    [Saiprasad]Added: Created Queue and added method enqueue

---
+++ test/java/MyQueueTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+public class MyQueueTest {
+    @Test
+    public void Adding3ElementsInQueue() {
+        MyNode<Integer> myFirstNode = new MyNode<Integer>(56);
+        MyNode<Integer> mySecondNode = new MyNode<Integer>(30);
+        MyNode<Integer> myThirdNode = new MyNode<Integer>(70);
+        MyQueue myQueue = new MyQueue();
+        myQueue.enqueue(myFirstNode);
+        myQueue.enqueue(mySecondNode);
+        myQueue.enqueue(myThirdNode);
+        myQueue.printQueue();
+    }
+}
